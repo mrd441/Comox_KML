3f54e02d45231e8772c07716fb5590144875dc79
mrd441 <441-05@mail.ru>
2022-03-13 13:38:41 +0300

    multithreading

--- Comox_KML/Form1.Designer.cs
+++ Comox_KML/Form1.Designer.cs
@@ -29,15 +29,27 @@ namespace Comox_KML
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.injFrag = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.elNameToInjAfter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 33);
+            this.button1.Location = new System.Drawing.Point(18, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,44 +59,143 @@ namespace Comox_KML
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 240);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 19);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(800, 128);
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(557, 167);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(107, 32);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(683, 192);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 38);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Проверить синтакс XML";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBox2.Location = new System.Drawing.Point(0, 144);
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox2.Location = new System.Drawing.Point(3, 19);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(800, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(273, 167);
             this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "<Style id=\"file:ПСП.zip:grn-pushpin.webp1\">\n<Style id=\"file:ПСП.zip:grn-pushpin.w" +
-    "ebp2\">";
+            this.richTextBox2.Text = "file:ПСП.zip:grn-pushpin.webp1\nfile:ПСП.zip:grn-pushpin.webp2";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.Location = new System.Drawing.Point(3, 19);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(674, 214);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            // 
+            // injFrag
+            // 
+            this.injFrag.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.injFrag.AutoSize = true;
+            this.injFrag.Checked = true;
+            this.injFrag.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.injFrag.Location = new System.Drawing.Point(683, 22);
+            this.injFrag.Name = "injFrag";
+            this.injFrag.Size = new System.Drawing.Size(135, 19);
+            this.injFrag.TabIndex = 5;
+            this.injFrag.Text = "Добавить фрагмент";
+            this.injFrag.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 283);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 189);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список id стилей для пропуска";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(294, 283);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(563, 189);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Консоль вывода";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.elNameToInjAfter);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.richTextBox3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.injFrag);
+            this.groupBox3.Location = new System.Drawing.Point(15, 41);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(839, 236);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Xml фрагмент для добавления";
+            // 
+            // elNameToInjAfter
+            // 
+            this.elNameToInjAfter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.elNameToInjAfter.Location = new System.Drawing.Point(683, 62);
+            this.elNameToInjAfter.Name = "elNameToInjAfter";
+            this.elNameToInjAfter.Size = new System.Drawing.Size(150, 23);
+            this.elNameToInjAfter.TabIndex = 7;
+            this.elNameToInjAfter.Text = "name";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(683, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "после элемента";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 368);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(869, 484);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,6 +206,14 @@ namespace Comox_KML
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.CheckBox injFrag;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox elNameToInjAfter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Comox_KML/Form1.cs
+++ Comox_KML/Form1.cs
@@ -5,10 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
+using System.Collections.Concurrent;
 
 using System.IO;
 using System.Diagnostics;
@@ -17,92 +19,190 @@ namespace Comox_KML
 {
     public partial class Form1 : Form
     {
+        public XElement xmlFrag;
+        public bool toInjRes;
+        public string[] excStyleList;
+        public string elNameToInjAfterT;
+        public CancellationTokenSource tokenSource ;
+
         public Form1()
         {
             InitializeComponent();
-        }
+            toInjRes = false;
+        }        
 
-        private void button1_Click(object sender, EventArgs e)
+        private void processFile(string filePath)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            var filePath = string.Empty;
-
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                //openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "txt files (*.kml)|*.kml|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = false;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                    filePath = openFileDialog.FileName;
-            }
-            if (filePath == string.Empty) return;
+            //loging(Task.CurrentId.ToString());
+            //loging("1");
+            XDocument xml = XDocument.Load(filePath, LoadOptions.SetBaseUri);
+            
             XNamespace ns = "http://www.opengis.net/kml/2.2";
-            richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
-            //XmlDocument doc = new XmlDocument();
-            XDocument xml = XDocument.Load(filePath);
-            //xml.AddBeforeSelf("<?xml version=\"1.0\" encoding=\"UTF - 8\"?>");
-            //LoadOptions asd = new LoadOptions();
-            richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
-            //try
-            //{
-            //    richTextBox1.AppendText((xml.Descendants(ns + "Style")
-            //                            .Where(x => (string)x.Attribute("id") == "file:ПСП.zip:grn-pushpin.webp")).First().Value);
-            //}
-            //catch (Exception) { }
-            richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
+            foreach (XAttribute xA in xml.Root.Attributes())
+            {
+                if (xA.Name == "xmlns")
+                {
+                    ns = xA.Value;
+                    XAttribute foo = xA;
+                    xA.Remove();
+                    xml.Root.Add(foo);
+                    break;
+                }
+            }
 
             xml.Descendants(ns + "Style")
-            .Where(x =>  !richTextBox2.Lines.Contains((string)x.Attribute("id")))
+            .Where(x => !excStyleList.Contains((string)x.Attribute("id")))
             .Remove();
-            xml.Declaration = new XDeclaration("1.0", "UTF-8", null);
-            //var output = xml.ToString().Replace("kml:", string.Empty).Replace("    ", );
+                        
+            if (toInjRes)
+            {
+                foreach (XElement el in xml.Descendants(ns + elNameToInjAfterT))
+                {
+                    el.AddAfterSelf(xmlFrag);
+                }
+            }
+            //xml.Descendants(ns + "name").First().AddAfterSelf(xmlTree);
 
-            //XmlDocumentFragment xfrag = doc.CreateDocumentFragment();
-
-            //xfrag.InnerXml = "<Style id =\"file: ПСП\"><IconStyle ><Icon ><href > files /file - ПСП.zip - grn - pushpin.webp </href ></Icon ><hotSpot x = \"0.5\" y = \"0\" xunits = \"fraction\" yunits = \"fraction\" /> </IconStyle ></Style > ";
-            //xml.Descendants(ns + "name").First().AddAfterSelf(xfrag);
             XmlWriterSettings settings = new XmlWriterSettings();
             settings.Indent = true;
             settings.IndentChars = ("\t");
-            settings.OmitXmlDeclaration = true;
+            settings.OmitXmlDeclaration = false;
             settings.Encoding = new UTF8Encoding(false);
+            settings.NamespaceHandling = NamespaceHandling.OmitDuplicates;
 
-            StringBuilder sb = new StringBuilder();
-            XmlWriter writer = XmlWriter.Create(sb, settings);
-           
-            xml.Save(writer);
-            writer.Dispose();
-            //richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
-            sb.Insert(0,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-            sb.Replace("kml:", string.Empty);
-            //richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
-            File.WriteAllText(filePath + "1", sb.ToString());
-            //richTextBox1.AppendText(watch.Elapsed.TotalSeconds.ToString());
-            watch.Stop();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            var filePath = string.Empty;
-
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            using (XmlWriter w = XmlWriter.Create(filePath + "1", settings))
             {
-                //openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "txt files (*.kml)|*.kml|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = false;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                    filePath = openFileDialog.FileName;
+                xml.Save(w);
             }
-            if (filePath == string.Empty) return;
 
-
-            
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            elNameToInjAfterT = elNameToInjAfter.Text;
+            //elNameToInjAfter.Invoke(new MethodInvoker(() => elNameToInjAfterT = elNameToInjAfter.Text));
+            excStyleList = richTextBox2.Lines;
+            toInjRes = injFrag.Checked;
+
+            if (toInjRes && elNameToInjAfterT=="")
+            {
+                loging("Имя элемента после, которого необходимо необходимо добавить фрагмент, не может быть пустым.", 2);
+                return;
+            }
+
+            if (toInjRes && !checkXmlFrag())
+                return;  
+
+            //var filePath = string.Empty;
+            //using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            //{
+            //    openFileDialog.Filter = "txt files (*.kml)|*.kml|All files (*.*)|*.*";
+            //    openFileDialog.FilterIndex = 2;
+            //    openFileDialog.RestoreDirectory = false;
+
+            //    if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //        filePath = openFileDialog.FileName;
+            //}
+            //if (filePath == string.Empty) return;
+
+            //await Task.Run(() => processFile(filePath));
+
+            loging("Подсчет фалов...");
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\akim\Downloads\Новая папка"); //Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.kml");
+            loging("Всего файлов - " + Files.Count().ToString());
+
+            tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            //var factory = new TaskFactory();
+            var tasks = new List<Task>();
+            loging("Начало.");
+            foreach (var file in Files)
+                //tasks.Add(factory.StartNew(() => p2(file.FullName), token, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default));
+                tasks.Add(Task.Run(() => p2(file.FullName), token));
+
+            try
+            {
+                await Task.WhenAll(tasks.ToArray());
+            }
+            catch (OperationCanceledException)
+            {
+                loging("Остановлено по причине " + nameof(OperationCanceledException));
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+            
+            loging("Завершено.");
+
+        }
+
+        private void p2(string ss)
+        {
+            loging(Task.CurrentId.ToString() + " - start");
+            Thread.Sleep(1000);
+            loging(Task.CurrentId.ToString() + " - stop");
+        }
+
+        private bool checkXmlFrag()
+        {
+            string fragT = "";
+            richTextBox3.Invoke(new MethodInvoker(()=>  fragT = richTextBox3.Text ));
+
+            try
+            {
+                xmlFrag = XElement.Parse(fragT);
+            }
+            catch (Exception ex)
+            {
+                loging("Ошибка в XML фрагменте. " + ex.Message,2);
+                return false;
+            }
+            return true;
+        }
+
+        private async void  button2_Click(object sender, EventArgs e)
+        {
+            if (await Task.Run(() => checkXmlFrag()))
+                loging("Ошибок не найдено.");
+        }
+
+        public void loging(string text, int level = 0)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action< string, int>(loging), new object[] { text, level });
+                return;
+            }
+            var aColor = Color.Black;
+            if (level == 1)
+                aColor = Color.Green;
+            else if (level == 2)
+                aColor = Color.Red;
+            string curentTime = DateTime.Now.TimeOfDay.ToString("hh\\:mm\\:ss");
+            richTextBox1.AppendText(curentTime + ": " + text + Environment.NewLine, aColor);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            loging("Завершение...");
+            tokenSource.Cancel();
+        }
+    }
+}
+
+public static class RichTextBoxExtensions
+{
+    public static void AppendText(this RichTextBox box, string text, Color color)
+    {
+        box.SelectionStart = box.TextLength;
+        box.SelectionLength = 0;
+
+        box.SelectionColor = color;
+        box.AppendText(text);
+        box.SelectionColor = box.ForeColor;
+        box.SelectionStart = box.Text.Length;
+        box.ScrollToCaret();
     }
 }
